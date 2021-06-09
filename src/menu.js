9220ee7d434c2a5e01eca4746316475a21bcee5f
Julija Faltina <dashinicha@gmail.com>
2021-06-09 22:20:03 +0300

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -10,3 +10,28 @@ function createMenuCard(menuCards) {
 const cardsMarkup = createMenuCard(menuCards);
 const ulElements = document.querySelector(".js-menu");
 ulElements.insertAdjacentHTML("beforeend", cardsMarkup);
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+
+const body = document.querySelector("body");
+
+const button = document.querySelector("#theme-switch-toggle");
+
+button.onchange = function () {
+  if (button.checked === true) {
+    body.classList.add("dark-theme");
+    localStorage.setItem("theme-color", Theme.DARK);
+  } else {
+    body.classList.remove("dark-theme");
+    localStorage.setItem("theme-color", Theme.LIGHT);
+  }
+};
+const activeTheme = localStorage.getItem("theme-color");
+
+if (activeTheme && activeTheme === Theme.DARK) {
+  button.checked = true;
+  body.classList.add("dark-theme");
+}
